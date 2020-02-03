b894c72d562fa6cd1ca5ce1a867a3056d46a4c35
Juan <sevi0_6@hotmail.com>
2020-02-03 20:06:37 +0100

    loading models with classes

--- src/js/app.js
+++ src/js/app.js
@@ -10,133 +10,132 @@ import {
   Clock,
 } from 'three'
 
+//import Stork from '../models/Stork.glb'
+//import Flamingo from '../models/Flamingo.glb'
+//import Parrot from '../models/Parrot.glb'
+
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 import '../css/main.css'
 
+class App {
 
-let container;
-let scene;
-let camera;
-let renderer;
-let controls;
+  constructor() {
 
-const mixers = [] // saving each model animation as an action = AnimationClip 
-const clock = new Clock() // Threejs clock to track animation's time 
+    this.scene = new Scene()
+    this.scene.background = new Color('blue')
 
 
-const init = () => {
-  container = document.createElement('div')
-  container.id = 'container'
-  document.querySelector('body').appendChild(container)
+    this.container = document.createElement('div')
+    this.container.id = 'container'
+    document.querySelector('body').appendChild(this.container)
 
-  scene = new Scene()
-  scene.background = new Color('blue')
+    this.mixers = [] // saving each model animation as an action = AnimationClip 
+    this.clock = new Clock() // Threejs clock to track animation's time 
 
-  createCamera()
-  createLight()
-  loadModels()
-  createControls()
-  createRenderer()
 
-  // GAME LOOP!
-  renderer.setAnimationLoop(() => {
-    update()
-    render()
-  })
-}
+    this.createCamera()
+    this.createLight()
+    this.loadModels()
+    this.createRenderer()
 
-const update = () => {
+    this.controls = new OrbitControls(this.camera, this.container)
 
-  // Time between frames
-  const delta = clock.getDelta()
-  for (const mixer of mixers) {
-    mixer.update(delta)
+    // GAME LOOP!
+    this.renderer.setAnimationLoop(() => {
+      this.update()
+      this.render()
+    })
+
+    // Adding resize listener 
+    window.addEventListener('resize', this.onWindowResize)
+  }
+
+  update() {
+    // Time between frames
+    const delta = this.clock.getDelta()
+    for (const mixer of this.mixers) {
+      mixer.update(delta)
+    }
+
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  loadModels() {
+    // Model loader 
+    const loader = new GLTFLoader()
+    // Callback once it's loaded
+    const onLoad = (gltf, position) => {
+      // Getting the model from the gltf file
+      const model = gltf.scene.children[0]
+      model.position.copy(position)
+      model.scale.set(0.01, 0.01, 0.01)
+      //Getting the animation from gltf file 
+      const animation = gltf.animations[0]
+      // Attaching animation to the model
+      const mixer = new AnimationMixer(model)
+      this.mixers.push(mixer)
+      // Creating the action reference (so you can play and stop the animation)
+      const action = mixer.clipAction(animation)
+      action.play()
+
+      this.scene.add(model)
+    }
+
+    const onProgress = () => console.log('loading model...')
+    const onError = error => console.log('error: ', error)
+
+    const parrotPosition = new Vector3(0, 0, 2.5)
+    loader.load('./models/Parrot.glb', glb => onLoad(glb, parrotPosition), onProgress, onError)
+
+    const flamingoPosition = new Vector3(-1.5, 0, 1)
+    loader.load('./models/Flamingo.glb', glb => onLoad(glb, flamingoPosition), onProgress, onError)
+
+    const storkPosition = new Vector3(0, -1.5, -1)
+    loader.load('./models/Stork.glb', glb => onLoad(glb, storkPosition), onProgress, onError)
+  }
+
+  createLight() {
+    const ambient = new HemisphereLight(0xddeeff, 0x0f0e0d, 5)
+    const light = new DirectionalLight(0xffffff, 5)
+    light.position.set(10, 10, 10)
+    this.scene.add(ambient, light)
+  }
+
+  createCamera() {
+
+    const fov = 35
+    const aspect = container.clientWidth / container.clientHeight
+    const near = 1
+    const far = 100
+    this.camera = new PerspectiveCamera(fov, aspect, near, far)
+    this.camera.position.set(-3, -1.5, 6.5)
+
+  }
+
+  createRenderer() {
+    this.renderer = new WebGLRenderer()
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+
+    this.renderer.gammaFactor = 2.2;
+    this.renderer.gammaOutput = true;
+    this.renderer.physicallyCorrectLights = true;
+
+    this.container.appendChild(this.renderer.domElement)
+  }
+
+  onWindowResize() {
+    this.camera.aspect = this.container.clientWidth / this.container.clientHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
   }
 
 }
 
-const loadModels = () => {
-  // Model loader 
-  const loader = new GLTFLoader()
-  // Callback once it's loaded
-  const onLoad = (gltf, position) => {
-    // Getting the model from the gltf file
-    const model = gltf.scene.children[0]
-    model.position.copy(position)
-    model.scale.set( 0.01, 0.01, 0.01)
-    //Getting the animation from gltf file 
-    const animation = gltf.animations[0]
-    // Attaching animation to the model
-    const mixer = new AnimationMixer(model)
-    mixers.push(mixer)
-    // Creating the action reference (so you can play and stop the animation)
-    const action = mixer.clipAction(animation)
-    action.play()
-
-    scene.add(model)
-  }
-
-  const onProgress = () => console.log('loading model...')
-  const onError = error => console.log('error: ', error)
-
-  const parrotPosition = new Vector3( 0, 0, 2.5)
-  loader.load('./models/Parrot.glb', glb => onLoad(glb, parrotPosition), onProgress, onError)
-
-  const flamingoPosition = new Vector3( -1.5, 0, 1)
-  loader.load('./models/Flamingo.glb', glb => onLoad(glb, flamingoPosition), onProgress, onError)
-
-  const storkPosition = new Vector3(0, -1.5, -1)
-  loader.load('./models/Stork.glb', glb => onLoad(glb, storkPosition), onProgress, onError)
-}
-
-const createCamera = () => {
-
-  const fov = 35
-  const aspect = container.clientWidth / container.clientHeight
-  const near = 1
-  const far = 100
-  camera = new PerspectiveCamera(fov, aspect, near, far)
-  camera.position.set(-3, -1.5, 6.5 )
-
-}
-
-const createLight = () => {
-  const ambient = new HemisphereLight(0xddeeff, 0x0f0e0d, 5)
-  const light = new DirectionalLight(0xffffff, 5)
-  light.position.set(10, 10, 10)
-  scene.add(ambient, light)
-}
-
-const createControls = () => {
-  controls = new OrbitControls(camera, container)
-}
-
-const createRenderer = () => {
-
-  renderer = new WebGLRenderer()
-  renderer.setSize(container.clientWidth, container.clientHeight)
-  renderer.setPixelRatio(window.devicePixelRatio)
-
-  renderer.gammaFactor = 2.2;
-  renderer.gammaOutput = true;
-  renderer.physicallyCorrectLights = true;
-
-  container.appendChild(renderer.domElement)
-
-}
-
-const render = () => {
-  renderer.render(scene, camera)
-}
-
-const onWindowResize = () => {
-  camera.aspect = container.clientWidth / container.clientHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(container.clientWidth, container.clientHeight)
-}
-
-window.addEventListener('resize', onWindowResize)
-// Start whole this
-init()
+const instance = new App();
+export default instance; 
---
+++ src/js/loadingModel.js
@@ -0,0 +1,143 @@
+import {
+    Scene,
+    PerspectiveCamera,
+    DirectionalLight,
+    HemisphereLight,
+    WebGLRenderer,
+    AnimationMixer,
+    Vector3,
+    Color,
+    Clock,
+  } from 'three'
+  
+  import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+  import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+  
+  import '../css/main.css'
+  
+  
+  let container;
+  let scene;
+  let camera;
+  let renderer;
+  let controls;
+  
+  const mixers = [] // saving each model animation as an action = AnimationClip 
+  const clock = new Clock() // Threejs clock to track animation's time 
+  
+  
+  const init = () => {
+    container = document.createElement('div')
+    container.id = 'container'
+    document.querySelector('body').appendChild(container)
+  
+    scene = new Scene()
+    scene.background = new Color('blue')
+  
+    createCamera()
+    createLight()
+    loadModels()
+    createControls()
+    createRenderer()
+  
+    // GAME LOOP!
+    renderer.setAnimationLoop(() => {
+      update()
+      render()
+    })
+  }
+  
+  const update = () => {
+  
+    // Time between frames
+    const delta = clock.getDelta()
+    for (const mixer of mixers) {
+      mixer.update(delta)
+    }
+  
+  }
+  
+  const loadModels = () => {
+    // Model loader 
+    const loader = new GLTFLoader()
+    // Callback once it's loaded
+    const onLoad = (gltf, position) => {
+      // Getting the model from the gltf file
+      const model = gltf.scene.children[0]
+      model.position.copy(position)
+      model.scale.set( 0.01, 0.01, 0.01)
+      //Getting the animation from gltf file 
+      const animation = gltf.animations[0]
+      // Attaching animation to the model
+      const mixer = new AnimationMixer(model)
+      mixers.push(mixer)
+      // Creating the action reference (so you can play and stop the animation)
+      const action = mixer.clipAction(animation)
+      action.play()
+  
+      scene.add(model)
+    }
+  
+    const onProgress = () => console.log('loading model...')
+    const onError = error => console.log('error: ', error)
+  
+    const parrotPosition = new Vector3( 0, 0, 2.5)
+    loader.load('./models/Parrot.glb', glb => onLoad(glb, parrotPosition), onProgress, onError)
+  
+    const flamingoPosition = new Vector3( -1.5, 0, 1)
+    loader.load('./models/Flamingo.glb', glb => onLoad(glb, flamingoPosition), onProgress, onError)
+  
+    const storkPosition = new Vector3(0, -1.5, -1)
+    loader.load('./models/Stork.glb', glb => onLoad(glb, storkPosition), onProgress, onError)
+  }
+  
+  const createCamera = () => {
+  
+    const fov = 35
+    const aspect = container.clientWidth / container.clientHeight
+    const near = 1
+    const far = 100
+    camera = new PerspectiveCamera(fov, aspect, near, far)
+    camera.position.set(-3, -1.5, 6.5 )
+  
+  }
+  
+  const createLight = () => {
+    const ambient = new HemisphereLight(0xddeeff, 0x0f0e0d, 5)
+    const light = new DirectionalLight(0xffffff, 5)
+    light.position.set(10, 10, 10)
+    scene.add(ambient, light)
+  }
+  
+  const createControls = () => {
+    controls = new OrbitControls(camera, container)
+  }
+  
+  const createRenderer = () => {
+  
+    renderer = new WebGLRenderer()
+    renderer.setSize(container.clientWidth, container.clientHeight)
+    renderer.setPixelRatio(window.devicePixelRatio)
+  
+    renderer.gammaFactor = 2.2;
+    renderer.gammaOutput = true;
+    renderer.physicallyCorrectLights = true;
+  
+    container.appendChild(renderer.domElement)
+  
+  }
+  
+  const render = () => {
+    renderer.render(scene, camera)
+  }
+  
+  const onWindowResize = () => {
+    camera.aspect = container.clientWidth / container.clientHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(container.clientWidth, container.clientHeight)
+  }
+  
+  window.addEventListener('resize', onWindowResize)
+  // Start whole this
+  init()
+  
